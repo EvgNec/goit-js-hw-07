314f903b25d1791d47ab9b3c4245d43955cbfb4d
Evgeniy <evgnec@gmail.com>
2023-03-26 20:19:08 +0300

    add ex1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-
-
-
 const gallery = document.querySelector('.gallery');
 
 const galleryIt = galleryItems.map(function ({ preview, original, description }) {
     return `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
+     <a class="gallery__link" href="${original}" target="_blank">
         <img 
     class = "gallery__image"
     src="${preview}"
@@ -23,5 +19,30 @@ const galleryIt = galleryItems.map(function ({ preview, original, description })
 
 gallery.insertAdjacentHTML('beforeend', galleryIt);
 
+gallery.addEventListener('click', clickImage);
+
+
+function clickImage(event) {
+
+    event.preventDefault();
+ 
+    const imgInstance = event.target;
+    if (imgInstance.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img class="gallery__image" src="${imgInstance.dataset.source}" width="800" height="600">
+`)
+    instance.show();
+ window.addEventListener('keydown', onCloseModalClick);
+}
+
+function onCloseModalClick(event) {
+    const openInstance = document.querySelector(".basicLightbox")
+    if (event.code === "Escape") {
+        openInstance.remove();
+    }
+    window.removeEventListener('keydown', onCloseModalClick);  
+}
 
 
